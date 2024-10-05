f06866953ce113797aeab5ece5ac8959d33549bc
Anushka <anushka.v@berkeley.edu>
2024-10-05 15:13:19 -0700

    basic layout readywith flexbox

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 const search = document.querySelector('.search');
-search.style.backgroundColor = 'red';
 
 const temp = document.querySelector('.temperature');
 temp.addEventListener("click", function() {
@@ -10,5 +9,5 @@ const input = document.querySelector('.input-copy-from')
 const paragraph = document.querySelector('.input-copy-to')
 const paraContainer = document.querySelector('.search')
 input.addEventListener('change', function() {
-    paraContainer.style.backgroundColor = input.value;
+    paragraph.textContent = input.value;
 })
