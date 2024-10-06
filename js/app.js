54b1e983a1b3443b1121ac9930b7dd06553967e3
Anushka <anushka.v@berkeley.edu>
2024-10-05 17:54:58 -0700

    basic weather api functionality (text only)

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
+// FUNCTIONS
+function getWeatherData(city) {
+    const apiKey = "47c2a0b267bb49e6949222002240510"
+    const apiURL = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`
+
+    fetch(apiURL)
+        .then(response => {
+            if (!response.ok) {
+                alert('Try entering a valid city name.')
+            }
+            return response.json();
+        })
+        .then(data => {
+            // update
+            const weather = document.querySelector('.weather-text')
+            const timezone = document.querySelector('.timezone-text')
+
+            weather.textContent = data.location.name + ", " + data.location.region + ", " + data.location.country
+            timezone.textContent = data.current.condition.text
+        })
+}
+
+// DOM ELEMENTS
+
+
+
 const search = document.querySelector('.search');
 
 const temp = document.querySelector('.temperature');
@@ -5,9 +31,14 @@ temp.addEventListener("click", function() {
     alert('well oo man the way they turn cold');
 });
 
-const input = document.querySelector('.input-copy-from')
-const paragraph = document.querySelector('.input-copy-to')
-const paraContainer = document.querySelector('.search')
-input.addEventListener('change', function() {
-    paragraph.textContent = input.value;
+const rawInput = document.querySelector('.input')
+const paraContainer = document.querySelector('.search-box')
+rawInput.addEventListener('keydown', function(event) {
+    if ((event.key ) == 'Enter')  {
+        const input = rawInput.value.trim();
+        if (input != "") {
+            // call weather function
+            getWeatherData(input)
+        }
+    }
 })
