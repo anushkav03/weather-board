5a83c1bdca3f26ae1b9c42fb23ed37bf6418fa30
Anushka <anushka.v@berkeley.edu>
2024-11-06 12:21:54 -0800

    port fix

--- app.py
+++ app.py
@@ -32,5 +32,8 @@ def get_weather(city=None):
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
